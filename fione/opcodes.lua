6d9270c0d594331c3573f2784f648a924b6b053d
LOZENP <zeniaorcalero@gmail.com>
2025-12-28 04:28:01 +0800

    Create opcodes.lua

---
+++ fione/opcodes.lua
@@ -0,0 +1,110 @@
+--[[
+Lua opcode definitions and mappings
+]]--
+
+-- SETLIST config
+local FIELDS_PER_FLUSH = 50
+
+-- Remap for better lookup
+local OPCODE_RM = {
+	-- level 1
+	[22] = 18, -- JMP
+	[31] = 8, -- FORLOOP
+	[33] = 28, -- TFORLOOP
+	-- level 2
+	[0] = 3, -- MOVE
+	[1] = 13, -- LOADK
+	[2] = 23, -- LOADBOOL
+	[26] = 33, -- TEST
+	-- level 3
+	[12] = 1, -- ADD
+	[13] = 6, -- SUB
+	[14] = 10, -- MUL
+	[15] = 16, -- DIV
+	[16] = 20, -- MOD
+	[17] = 26, -- POW
+	[18] = 30, -- UNM
+	[19] = 36, -- NOT
+	-- level 4
+	[3] = 0, -- LOADNIL
+	[4] = 2, -- GETUPVAL
+	[5] = 4, -- GETGLOBAL
+	[6] = 7, -- GETTABLE
+	[7] = 9, -- SETGLOBAL
+	[8] = 12, -- SETUPVAL
+	[9] = 14, -- SETTABLE
+	[10] = 17, -- NEWTABLE
+	[20] = 19, -- LEN
+	[21] = 22, -- CONCAT
+	[23] = 24, -- EQ
+	[24] = 27, -- LT
+	[25] = 29, -- LE
+	[27] = 32, -- TESTSET
+	[32] = 34, -- FORPREP
+	[34] = 37, -- SETLIST
+	-- level 5
+	[11] = 5, -- SELF
+	[28] = 11, -- CALL
+	[29] = 15, -- TAILCALL
+	[30] = 21, -- RETURN
+	[35] = 25, -- CLOSE
+	[36] = 31, -- CLOSURE
+	[37] = 35, -- VARARG
+}
+
+-- Opcode types for getting values
+local OPCODE_T = {
+	[0] = 'ABC', 'ABx', 'ABC', 'ABC', 'ABC', 'ABx', 'ABC', 'ABx',
+	'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'ABC',
+	'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'AsBx', 'ABC',
+	'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'ABC', 'AsBx',
+	'AsBx', 'ABC', 'ABC', 'ABC', 'ABx', 'ABC',
+}
+
+local OPCODE_M = {
+	[0] = {b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgR'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgR', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgN', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgN', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+}
+
+return {
+	FIELDS_PER_FLUSH = FIELDS_PER_FLUSH,
+	OPCODE_RM = OPCODE_RM,
+	OPCODE_T = OPCODE_T,
+	OPCODE_M = OPCODE_M,
+}
